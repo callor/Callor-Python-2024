5f30a3b27033a2872ccc82bf01a2cf7f312428b7
callor <callor@callor.com>
2023-12-05 19:15:39 +0900

    Hello2

---
+++ Hello_02/HelloA.py
@@ -0,0 +1,14 @@
+name = input("이름을 입력하세요")
+print(name,"님 환영합니다 ")
+
+name = input("이름을 입력하세요")
+print(name,"님 환영합니다 ")
+
+name = input("이름을 입력하세요")
+print(name,"님 환영합니다 ")
+
+name = input("이름을 입력하세요")
+print(name,"님 환영합니다 ")
+
+name = input("이름을 입력하세요")
+print(name,"님 환영합니다 ")
---
+++ Hello_02/HelloB.py
@@ -0,0 +1,6 @@
+i = 0
+while i < 5 :
+    name = input("이름을 입력하세요 >> ")
+    print(name,"님 환영합니다 ")
+    i = i + 1
+print("종료합니다")    
